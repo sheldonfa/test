e190ed3a6b574000532a6116ba47f819db977aae
fangxin <727171280@qq.com>
2017-06-18 10:22:51 +0800

    Initial commit

---
+++ src/main/java/com/Hello.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * @author fangxin
+ * @date 2017/6/18.
+ */
+public class Hello {
+
+    public void run(){
+        System.out.println("aaa");
+    }
+
+    public void run2(){
+        System.out.println("aaaa");
+    }
+}
---
+++ src/test/java/com/HelloTest.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author fangxin
+ * @date 2017/6/18.
+ */
+public class HelloTest {
+
+    @Test
+    public void testRun() throws Exception {
+        int i = 1 / 0;
+    }
+
+    @Test
+    public void testRun2() throws Exception {
+
+    }
+}
